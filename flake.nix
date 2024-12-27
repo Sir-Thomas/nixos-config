109995e8014a1f7a636931b7db1e447edc8230eb
Sir-Thomas <tp8153@gmail.com>
2024-12-26 22:11:37 -0600

    fix hostname

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   };
   outputs = inputs@{ self, nixpkgs, ... }: {
     # NOTE: 'nixos' is the default hostname set by the installer
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.tommy-laptop = nixpkgs.lib.nixosSystem {
       # NOTE: Change this to aarch64-linux if you are on ARM
       system = "x86_64-linux";
       modules = [ 
