73d197639805af45b31621ac2490bd430da34afd
Sir-Thomas <tp8153@gmail.com>
2024-12-26 19:07:03 -0600

    remap caps to esc and add moonlight

--- configuration.nix
+++ configuration.nix
@@ -56,6 +56,24 @@
     xkbVariant = "";
   };
 
+  # Remap CAPS to ESC/CTRL
+  services.interception-tools =
+  let
+    itools = pkgs.interception-tools;
+    itools-caps = pkgs.interception-tools-plugins.caps2esc;
+  in
+  {
+    enable = true;
+    plugins = [ itools-caps ];
+    # requires explicit paths: https://github.com/NixOS/nixpkgs/issues/126681
+    udevmonConfig = pkgs.lib.mkDefault ''
+      - JOB: "${itools}/bin/intercept -g $DEVNODE | ${itools-caps}/bin/caps2esc -m 1 | ${itools}/bin/uinput -d $DEVNODE"
+        DEVICE:
+          EVENTS:
+            EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+    '';
+  };
+
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
@@ -105,6 +123,7 @@
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
+    moonlight-qt
   ];
   
   services.fprintd.enable = true;
