3ba63ee7695790121e9aea4374d68c1a9730e19f
Sir-Thomas <tp8153@gmail.com>
2024-12-31 22:41:27 -0600

    add udev rules for st-link v2

--- configuration.nix
+++ configuration.nix
@@ -140,6 +140,7 @@
   # Set udev rules for micro bit
   services.udev.extraRules = ''
     SUBSYSTEM=="usb", ATTR{idVendor}=="0d28", ATTR{idProduct}=="0204", MODE="0666"
+    SUBSYSTEM=="usb", ATTR{idVendor}=="0483", ATTR{idProduct}=="3748", MODE="0666"
   '';
 
   # Some programs need SUID wrappers, can be configured further or are
