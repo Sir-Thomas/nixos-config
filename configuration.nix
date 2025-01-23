0a85455043385e063bfccc67dea6252ef7ec17bc
Sir-Thomas <tp8153@gmail.com>
2025-01-23 15:05:38 -0600

    fix scaling on sddm

--- configuration.nix
+++ configuration.nix
@@ -50,7 +50,11 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    enableHidpi = true;
+    wayland.enable = true;
+  };
   services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
